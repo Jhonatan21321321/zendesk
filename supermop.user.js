ef44929a6ad488dcb33d0075773c9da93871ce09
Jhonatan21321321 <jhojho_100@hotmail.com>
2025-10-02 16:01:55 -0300

    Update and rename supermop.js to supermop.user.js

--- supermop.user.js
+++ supermop.user.js
@@ -8,8 +8,8 @@
 // @grant        GM_xmlhttpRequest
 // @connect      docs.google.com
 // @run-at       document-idle
-// @updateURL    https://raw.githubusercontent.com/Jhonatan21321321/zendesk/main/supermop.js
-// @downloadURL  https://raw.githubusercontent.com/Jhonatan21321321/zendesk/main/supermop.js
+// @updateURL    https://raw.githubusercontent.com/Jhonatan21321321/zendesk/main/supermop.user.js
+// @downloadURL  https://raw.githubusercontent.com/Jhonatan21321321/zendesk/main/supermop.user.js
 // ==/UserScript==
 (function () {
     'use strict';
